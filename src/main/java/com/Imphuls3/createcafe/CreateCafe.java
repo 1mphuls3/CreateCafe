dbf231e28d4ae1530d27b0b86e7f62c47907fb6c
1mphuls3 <isaacmorrell13@gmail.com>
2022-08-13 22:09:49 -0700

    Fixed composting causing crash on startup

--- src/main/java/com/Imphuls3/createcafe/CreateCafe.java
+++ src/main/java/com/Imphuls3/createcafe/CreateCafe.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -18,36 +19,34 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(CreateCafe.MOD_ID)
-public class CreateCafe
-{
+public class CreateCafe {
     public static final String MOD_ID = "createcafe";
 
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public CreateCafe() {
-        // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::commonSetup);
         eventBus.addListener(this::doClientStuff);
 
         ItemRegistry.register(eventBus);
         FluidRegistry.register(eventBus);
         BlockRegistry.register(eventBus);
         ConfigRegistry.register();
-        CompostRegistry.register();
 
         Compat.init();
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
+    private void commonSetup(FMLCommonSetupEvent event) {
+        CreateCafe.LOGGER.debug("common setup");
+        CompostRegistry.register();
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
