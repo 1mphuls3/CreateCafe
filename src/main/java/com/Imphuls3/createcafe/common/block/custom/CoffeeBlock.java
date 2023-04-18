37e804a55855f8b78fa0f0d907e13c5fce0eb90a
1mphuls3 <isaacmorrell13@gmail.com>
2023-04-17 19:26:39 -0700

    Textures n stuff

--- src/main/java/com/Imphuls3/createcafe/common/block/custom/CoffeeBlock.java
+++ src/main/java/com/Imphuls3/createcafe/common/block/custom/CoffeeBlock.java
@@ -1,12 +1,41 @@
 package com.Imphuls3.createcafe.common.block.custom;
 
 import com.Imphuls3.createcafe.core.registry.ItemRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class CoffeeBlock extends CropBlock {
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 13.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
     public CoffeeBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public int getMaxAge() {
+        return 3;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        if(this.getAge(state) < 3) {
+            return SHAPE_BY_AGE[state.getValue(this.getAgeProperty())];
+        }
+        return SHAPE_BY_AGE[3];
+    }
+    @Override
+    protected int getBonemealAgeIncrease(Level pLevel) {
+        return super.getBonemealAgeIncrease(pLevel) / 3;
     }
 
     @Override
